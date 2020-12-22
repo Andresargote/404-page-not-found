0a1ca0947057460b745d736cbc1820ee6f07782e
Andres <andresargote2019@gmail.com>
2020-12-22 22:30:19 +0100

    404 NOT FOUND page finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
+
+import Image from "./img/Scarecrow.png";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>404 NOT FOUND</h1>
       </header>
+      <main className="App-main">
+          <section className="App-main__img">
+              <img src={Image} alt="Scarecrow image"/>
+          </section>
+          <section className="App-main__content">
+             <h2>I have a bad news for you</h2>
+             <p>The page you are looking for might be removed or is temporarily unavailable</p>
+             <button>BACK TO HOMEPAGE</button>
+          </section>
+      </main>
+      <footer className="App-footer"><p>Andres Argote @DevChallenges.io</p></footer>
     </div>
   );
 }
